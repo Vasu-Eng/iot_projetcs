2d08cee90e5f0c11556e39653a276f911154ce34
Vasudev <kesharwanivasudev@gmail.com>
2021-12-31 22:18:06 -0800

    I have commited my first program

---
+++ hello.py
@@ -0,0 +1,18 @@
+print("hello world")
+import os # this is called python modules which increases the code funtionality
+import flask # if file is not installed then type commond - " pip intall flask"
+         #in terminal
+
+# "playsound" module 
+class Exam() :
+        Collage = "SGSITS"
+        Section = "I"
+
+        def givePaper(self):
+            print("Ready to give the test")
+
+            return 0  
+Vasudev = Exam()
+print(f"The collage of the student is {Vasudev.Collage}")
+Vasudev.givePaper 
+
